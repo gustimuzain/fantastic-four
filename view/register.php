934d487c780af46a7a5550a8a9e8a2f7aa863a8e
gustimuzain <aprilandrigusti@gmail.com>
2019-12-11 20:34:43 +0700

    tambah view register

---
+++ view/register.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Daftar</title>
+        <?php include 'layout/head.php' ?>
+    </head>
+
+    <body>
+        
+    <?php include 'layout/navbar.php' ?>
+
+        <img src="image/fantasticFourIl.png" alt="" class="ilus">
+        <div class="container">
+            <div class="row card shadow p-3 mb-5 register">
+                <div class="">
+                    <form>
+                        <center><h5>Daftar Sekarang</h5></center>
+                        <div class="form-group">
+                            <label>Nama</label>
+                            <input type="text" class="form-control" name="username" required="required">
+                        </div>
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="email" class="form-control" name="email" required="required">
+                        </div>
+                        <div class="form-group">
+                            <label>Nomor Ponsel</label>
+                            <input type="tel" class="form-control" name="username" required="required">
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" class="form-control" name="myInput" id="password" required="required">
+                        </div>
+                        <div class="form-group">
+                            <label>Confirm Password</label>
+                            <input type="password" class="form-control" name="confirm_password" id="confirm_password" required="required">
+                            <span id='message'></span>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-block btn-lg">Daftar</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="text-center">Sudah punya akun? <a href=""><strong>Masuk</strong></a></div>
+            </div>
+        </div>
+    </body>
+</html>
