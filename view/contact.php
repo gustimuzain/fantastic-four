0627d685eb48e7f2521adb35611f8f9905b5d30d
gustimuzain <aprilandrigusti@gmail.com>
2019-12-11 20:30:14 +0700

    tambah view kontak

---
+++ view/contact.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Kontak</title>
+        <?php include 'layout/head.php' ?>
+    </head>
+
+    <body>
+        
+    <?php include 'layout/navbar.php' ?>
+        
+        <div class="container">
+            <div class="row">    		
+                <div class="col-sm-12">    			   			
+                    <h2 class="title text-center">Hubungi Kami</h2>    			    				    				
+                    <div class="contact-map">
+                        <div style="width: 100%">
+                            <iframe width="100%" height="600" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;coord=110.40847328782317, -7.76047945&amp;q=Jl.%20Ring%20Road+(Fantastic%20Four%20Rental%20Car)&amp;ie=UTF8&amp;t=&amp;z=13&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/coordinates.html">find my coordinates</a></iframe>
+                        </div><br/>
+                    </div>
+                </div>			 		
+            </div>    	
+             <div class="">  	<!--row -->
+                <div class="col-sm-4">
+                    <h2 class="title">Info</h2>
+                    <address>
+                        <p>Fantastic Four Rental Car</p>
+                        <p>Jl. Ring Road</p>
+                        <p>Yogyakarta, Indonesia</p>
+                    </address>
+                </div>    			
+            </div>	
+        </div>
+        
+        <?php include 'layout/footer.php' ?>
+        
+    </body>
+</html>
