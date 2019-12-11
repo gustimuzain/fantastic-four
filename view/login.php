f20af832fad5270a0c790c817baac23d9765c77a
gustimuzain <aprilandrigusti@gmail.com>
2019-12-11 20:33:26 +0700

    tambah view login

---
+++ view/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Fantastic Four</title>
+        <?php include 'layout/head.php' ?>
+    </head>
+
+    <body>
+        
+        <?php include 'layout/navbar.php' ?>
+
+        <img src="image/fantasticFourIl.png" alt="" class="ilus">
+        <div class="container">
+            <div class="row card shadow p-3 mb-5 register">
+                <div class="">
+                    <form>
+                        <center><h5>Masuk</h5></center>
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="email" class="form-control nama" name="email" required="required">
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" class="form-control pass" name="password" required="required">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary btn-block btn-lg">Daftar</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="text-center">Sudah punya akun? <a href=""><strong>Masuk</strong></a></div>
+            </div>
+        </div>
+    </body>
+</html>
