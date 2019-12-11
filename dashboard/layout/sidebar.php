16bd97c9f22246f9838416a4aafeb66ea9436e7a
gustimuzain <aprilandrigusti@gmail.com>
2019-12-11 22:44:22 +0700

    tambah view dashboard

---
+++ dashboard/layout/sidebar.php
@@ -0,0 +1,7 @@
+<div class="border-right sidenav " id="sidebar-wrapper">
+    <div class="sidebar-heading">Fantastic Four</div>
+      <div class="list-group list-group-flush">
+        <a href="#" class="list-group-item list-group-item-action">Dashboard</a>
+        <a href="#" class="list-group-item list-group-item-action">Chart</a>
+    </div>
+</div>
