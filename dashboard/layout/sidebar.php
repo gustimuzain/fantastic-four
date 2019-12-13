6289ee026ca47a05966df8d75c6d6e94ec697627
gustimuzain <aprilandrigusti@gmail.com>
2019-12-13 21:05:40 +0700

    edit

--- dashboard/layout/sidebar.php
+++
@@ -1,7 +0,0 @@
-<div class="border-right sidenav " id="sidebar-wrapper">
-    <div class="sidebar-heading">Fantastic Four</div>
-      <div class="list-group list-group-flush">
-        <a href="#" class="list-group-item list-group-item-action">Dashboard</a>
-        <a href="#" class="list-group-item list-group-item-action">Chart</a>
-    </div>
-</div>
